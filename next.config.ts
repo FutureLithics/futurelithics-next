2e349b9ed14453e90f2aa16d459413bf22ae5143
futurelithics <chad.denaux@gmail.com>
2025-05-16 12:32:48 -0400

    chore: turn off lint errors

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://lyricitriade.com/wp-content/uploads/**')],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
