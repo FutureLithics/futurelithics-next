aa70da11a015314a5e58a8512530516e2788b123
futurelithics <chad.denaux@gmail.com>
2025-05-30 10:39:09 -0400

    feature: add node and link tooltips

--- app/_components/charts/network/StratifiedNetworkChart.js
+++ app/_components/charts/network/StratifiedNetworkChart.js
@@ -16,6 +16,17 @@ class StratifiedNetworkChart extends BaseChart {
         
         // Then process data and initialize simulation
         this.stratifyData(data);
+        this.strengthArcScale();
+        this.setupSimulation();
+        this.drawNodesAndLinks();
+    }
+
+    strengthArcScale(){
+        console.log(this.links, "links")
+        const min_max = d3.extent(this.links, (d) => d.interactionStrength || 0);
+        this.arcScale = d3.scaleLinear()
+            .domain(min_max)
+            .range([1, 2]);
     }
 
     stratifyData(data) {
@@ -26,8 +37,6 @@ class StratifiedNetworkChart extends BaseChart {
         this.links = [];
 
         this.createLinks(data);
-        this.setupSimulation();
-        this.drawNodesAndLinks();
     }
 
     createLinks(data) {
@@ -52,8 +61,8 @@ class StratifiedNetworkChart extends BaseChart {
 
     setupSimulation() {
         this.simulation = d3.forceSimulation(this.nodes)
-            .force("link", d3.forceLink(this.links).id((d) => d.id))
-            .force("charge", d3.forceManyBody().strength(-100))
+            .force("link", d3.forceLink(this.links).id((d) => d.id).strength((d) => d.type == "myco" ? 0.002 : 0.02))
+            .force("charge", d3.forceManyBody().strength(-1))
             .force("center", d3.forceCenter(this.options.width / 2, this.options.height / 2))
             .on("tick", this.ticked);
     }
@@ -74,6 +83,14 @@ class StratifiedNetworkChart extends BaseChart {
         }
     }
 
+    determinLinkColor(d){
+        if (d.type == "myco") {
+            return this.color(d.source.data.type);
+        } else {
+            return "#bbb";
+        }
+    }
+
     drawNodesAndLinks() {
         // Create link elements
         this.linkElements = this.mainGroup.append("g")
@@ -82,8 +99,8 @@ class StratifiedNetworkChart extends BaseChart {
             .data(this.links)
             .enter()
             .append("line")
-            .attr("stroke", "#999")
-            .attr("stroke-width", 1);
+            .attr("stroke", d => this.determinLinkColor(d))
+            .attr("stroke-width", d => this.arcScale(d.interactionStrength || 0));
 
         // Create node elements
         this.nodeElements = this.mainGroup.append("g")
@@ -94,6 +111,8 @@ class StratifiedNetworkChart extends BaseChart {
             .append("circle")
             .attr("r", 5)
             .attr("fill", d => this.color(d.data.type))
+            .attr("stroke", "000")
+            .attr("stroke-width", 1)
             .call(d3.drag()
                 .on("start", this.dragstarted.bind(this))
                 .on("drag", this.dragged.bind(this))
@@ -102,6 +121,8 @@ class StratifiedNetworkChart extends BaseChart {
         // Add hover title
         this.nodeElements.append("title")
             .text(d => d.data.id);
+
+        this.setupTooltips();
     }
     
     dragstarted(event, d) {
@@ -119,6 +140,75 @@ class StratifiedNetworkChart extends BaseChart {
         if (!event.active) this.simulation.alphaTarget(0);
         d.fx = null;
         d.fy = null;
+    }
+
+    setupTooltips(){
+        this.createTooltip();
+
+        this.nodeElements
+            .on("mouseover", (e, d) => this.displayNodeTooltip(e, d))
+            .on("mouseout", (_) => this.hideNodeTooltip());
+
+        this.linkElements
+            .on("mouseover", (e, d) => this.displayLinkTooltip(e, d))
+            .on("mouseout", (_, d) => this.hideLinkTooltip(_, d));
+    }
+
+    displayNodeTooltip(e, d){
+        this.targetNode = d3.select(e.currentTarget);
+        this.targetNode.style("stroke", "darkred");
+
+        this.tooltip.transition().duration(200).style("opacity", 0.9);
+
+        this.tooltip
+            .html(this.nodeTooltipHtml(d))
+            .style("left", e.pageX + "px")
+            .style("top", e.pageY - 30 + "px");
+        
+    }
+
+    nodeTooltipHtml(d){
+        return `
+            <strong>Name:</strong> ${d.data.id} <br />
+            <strong>Type:</strong> ${d.data.type} <br />
+            <strong>Parent:</strong> ${d.data.parent}
+        `;
+    }
+
+    hideNodeTooltip(){
+        this.targetNode.style("stroke", "none");
+        this.targetNode = null;
+
+        this.tooltip.transition().duration(200).style("opacity", 0);
+    }
+
+    displayLinkTooltip(e, d){
+        this.targetLink = d3.select(e.currentTarget);
+        this.targetLink.style("stroke", "blue");
+
+        this.tooltip.transition().duration(200).style("opacity", 1);
+
+        this.tooltip
+            .html(this.linkTooltipHtml(d))
+            .style("left", e.pageX + "px")
+            .style("top", e.pageY - 30 + "px");
+    }
+
+    hideLinkTooltip(e, d){
+        this.targetLink.style("stroke", d => this.determinLinkColor(d));
+        this.targetLink = null;
+
+        this.tooltip.transition().duration(200).style("opacity", 0);
+    }
+
+    linkTooltipHtml(d){
+        return `
+            <strong>Source:</strong> ${d.source.data.id} <br />
+            <strong>Target:</strong> ${d.target.data.id} <br />
+            <strong>Type:</strong> ${d.type} <br />
+            ${d.interactionStrength ? `<strong>Strength:</strong> ${d.interactionStrength} <br />` : ""}
+            ${d.benefits ? `<strong>Benefits:</strong><div> ${d.benefits?.join("<br />")}</div>` : ""}
+        `;
     }
 }
 
